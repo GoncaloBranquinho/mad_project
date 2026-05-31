fa832666fa7ed10295e17a648ee006c0b8709929
asj1n <190189908+asj1n@users.noreply.github.com>
2026-05-31 05:05:30 +0100

    Change .sh file

--- src/ex2/IP-ORTools/run-ip-ortools.sh
+++ src/ex2/IP-ORTools/run-ip-ortools.sh
@@ -5,5 +5,18 @@ fullPathToInputFile=$inputFilesPath$inputFileName
 if [ ! -f "$fullPathToInputFile" ]; then
     printf "$inputFilesPath file doesn't exist\n"
 else
+
+    inputFilesImagesPath="../../../inputs/input-files-images/"
+    read -p "Type 'y' to preview input partition image (any other input otherwise): " inputAnswer
+    fullPathToInputFileImage=$inputFilesImagesPath$inputFileName.pdf
+
+    if [ "$inputAnswer" == "y" ]; then
+        if [ -f "$fullPathToInputFileImage" ]; then
+            open -a Preview $fullPathToInputFileImage 
+        else
+            printf "$inputFileName.pdf image file doesn't exist\n"
+        fi
+    fi
+
     ./build/main $fullPathToInputFile
 fi
